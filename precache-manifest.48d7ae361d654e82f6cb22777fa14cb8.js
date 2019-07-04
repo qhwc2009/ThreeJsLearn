679b2519e563b0746d24882c8a73d04d63c97d7b
wangchen <2090861117@qq.com>
2019-07-05 00:46:18 +0800

    Updates

---
+++ precache-manifest.48d7ae361d654e82f6cb22777fa14cb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bbed1d094bf1386a8f87a5f7467ae9",
+    "url": "/ThreeJsLearn/index.html"
+  },
+  {
+    "revision": "dda5aa8903a5cf30bff0",
+    "url": "/ThreeJsLearn/static/css/2.9588f995.chunk.css"
+  },
+  {
+    "revision": "c793a5c23efd4668ce6e",
+    "url": "/ThreeJsLearn/static/css/main.a1925d2f.chunk.css"
+  },
+  {
+    "revision": "dda5aa8903a5cf30bff0",
+    "url": "/ThreeJsLearn/static/js/2.ae8f6101.chunk.js"
+  },
+  {
+    "revision": "c793a5c23efd4668ce6e",
+    "url": "/ThreeJsLearn/static/js/main.58bd6b54.chunk.js"
+  },
+  {
+    "revision": "0a9b381d890691e1165d",
+    "url": "/ThreeJsLearn/static/js/runtime~main.a61b8fef.js"
+  },
+  {
+    "revision": "952772476e630e2b1110859b3ca7cfc3",
+    "url": "/ThreeJsLearn/static/media/2.95277247.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d3db490dcf4cfbe71639aa18294e55f.js"
+  "/ThreeJsLearn/precache-manifest.48d7ae361d654e82f6cb22777fa14cb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ThreeJsLearn/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
